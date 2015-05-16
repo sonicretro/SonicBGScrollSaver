b47d2e515a63f70ca97a2165721ce5275ca16209
MainMemory <sonicmike2@yahoo.com>
2015-05-16 16:25:25 -0500

    Why does it keep missing these?

---
+++ Generic/Generic.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SonicRetro.SonLVL.API;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Generic
+{
+	public class Generic : SonicBGScrollSaver.Level
+	{
+		int[] Horiz_Scroll_Buf;
+		int Camera_X_pos, Camera_Y_pos;
+		BitmapBits levelimg;
+		Bitmap bgimg = new Bitmap(1, 1);
+		int Width, Height;
+		LevelInfo levelinfo;
+
+		public override void Init(int width, int height)
+		{
+			Width = width;
+			Height = height;
+			levelinfo = IniSerializer.Deserialize<LevelInfo>("setup.ini");
+			if (string.IsNullOrEmpty(levelinfo.Image))
+			{
+				LevelData.LoadGame("./setup.ini");
+				LevelData.LoadLevel("Level", true);
+				levelimg = LevelData.DrawBackground(null, true, true, false, false);
+			}
+			else
+				using (Bitmap tmp = new Bitmap(levelinfo.Image))
+				{
+					LevelData.BmpPal = tmp.Palette;
+					levelimg = new BitmapBits(tmp);
+				}
+			Horiz_Scroll_Buf = new int[levelimg.Height];
+			Camera_X_pos = 0;
+			UpdateScrolling(0, 0);
+		}
+
+		public override Bitmap GetBG()
+		{
+			return bgimg;
+		}
+
+		public override void UpdateScrolling(short Camera_X_pos_diff, short Camera_Y_pos_diff)
+		{
+			lock (bgimg)
+			{
+				Camera_X_pos += Camera_X_pos_diff;
+				Camera_Y_pos += Camera_Y_pos_diff;
+				BitmapBits bmp = new BitmapBits(levelimg);
+				bmp.ScrollVertical(Camera_Y_pos);
+				if (Height < bmp.Height)
+					bmp = bmp.GetSection(0, 0, bmp.Width, Height);
+				for (int i = 0; i < Horiz_Scroll_Buf.Length; i++)
+					Horiz_Scroll_Buf[i] = (int)(Camera_X_pos * levelinfo.HScrollSpeeds[(i + Camera_Y_pos) % levelinfo.HScrollSpeeds.Count]);
+				bmp.ScrollHorizontal((int[])Horiz_Scroll_Buf.Clone());
+				if (Width < bmp.Width)
+					bmp = bmp.GetSection(0, 0, Width, bmp.Height);
+				bgimg = bmp.ToBitmap(LevelData.BmpPal);
+			}
+		}
+
+		public override void PlayMusic()
+		{
+			if (!string.IsNullOrEmpty(levelinfo.Music))
+				SonicBGScrollSaver.Music.PlaySong(levelinfo.Music);
+		}
+	}
+
+	internal class LevelInfo
+	{
+		[IniName("music")]
+		public string Music { get; set; }
+		[IniName("image")]
+		public string Image { get; set; }
+		[IniName("hscroll")]
+		[IniCollection(IniCollectionMode.NoSquareBrackets)]
+		public List<double> HScrollSpeeds { get; set; }
+	}
+}
